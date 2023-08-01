1b1386bd7a46469f8867a6193fa1b58ced9caf8b
HendricksK <kurvinhendricks@gmail.com>
2023-08-01 12:33:10 +0200

    adding image alts and adapting gallery js to add alt

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -17,7 +17,7 @@ fetch('./assets/json/images.json')
     let images = json
 
     images.data.images.forEach((image, index) => {
-      gallery += `<a href="#img` + index + `"><img src="` + image.url + `"></a><a href="#" class="lightbox" id="img` + index + `"><span style="background-image: url('` + image.url + `')"></span></a>`
+      gallery += `<a href="#img` + index + `"><img src="` + image.url + `" alt="` + image.alt + `"></a><a href="#" class="lightbox" id="img` + index + `"><span style="background-image: url('` + image.url + `')"></span></a>`
     })
 
   document.getElementById('gallery').innerHTML = gallery
